e70218a74f48879a8ff91d5ecfb9e1bdec8a4642
NihalKass <pappu.kr26@gmail.com>
2018-03-20 23:09:09 +0530

    median of 3 no.

---
+++ median.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c,median;
+	printf("Enter any three no.");
+	scanf("%f%f%f",&a,&b,&c);
+	median=b;
+	printf("%f is the median among these three no. %f,%f,%f",b,a,b,c);
+	return 0;
+}
+
+
